a63b500a478c02be8917d918d084fd18887dc619
LazyCoder16 <78714319+LazyCoder16@users.noreply.github.com>
2021-02-19 18:24:33 +0530

    Updates

---
+++ precache-manifest.6fe83a5281f85d2bc8f90738a1c2015a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e76639e12241fd3d77dab5fdfb4df61",
+    "url": "/connect4-react-mcts/index.html"
+  },
+  {
+    "revision": "fd57b4cdf4f34ebb3030",
+    "url": "/connect4-react-mcts/static/css/main.7619a1e3.chunk.css"
+  },
+  {
+    "revision": "3f4953d4409fd3e8e444",
+    "url": "/connect4-react-mcts/static/js/2.8d4175d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/connect4-react-mcts/static/js/2.8d4175d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd57b4cdf4f34ebb3030",
+    "url": "/connect4-react-mcts/static/js/main.91f40d89.chunk.js"
+  },
+  {
+    "revision": "fd28e561d026ef52bf87",
+    "url": "/connect4-react-mcts/static/js/runtime-main.03aa3558.js"
+  }
+]);
